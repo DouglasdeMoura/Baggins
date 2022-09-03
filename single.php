0d9c1a6de581b0c2281928de07fb7f2625e0b6fb
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-02 23:05:49 -0300

    chore: set layout for the main content

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -9,32 +10,35 @@
 
 get_header();
 ?>
+	<div class="container">
+		<div class="columns">
+			<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'baggins' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'baggins' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'baggins' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'baggins' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				endwhile; // End of the loop.
+				?>
 
-		endwhile; // End of the loop.
-		?>
+			</main><!-- #main -->
 
-	</main><!-- #main -->
-
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
 <?php
-get_sidebar();
 get_footer();
