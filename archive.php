e69462550c39189fcd1b39d56e85c95015717a49
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-03 02:19:08 -0300

    fix: archive markup

--- archive.php
+++ archive.php
@@ -9,43 +9,46 @@
 
 get_header();
 ?>
+	<div class="container">
+		<div class="columns">
+			<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+				<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+					<header class="page-header">
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', get_post_type() );
+
+					endwhile;
+
+					the_posts_navigation();
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
 				?>
-			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			</main><!-- #main -->
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
+	<?php get_sidebar(); ?>
+		</div>
+	</div>
 <?php
-get_sidebar();
 get_footer();
